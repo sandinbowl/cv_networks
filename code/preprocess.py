18657d00e6ac5f607353d2f49da73de450bed99f
Yian17 <ajxxstella@gmail.com>
2020-07-02 12:53:13 -0400

    preprocess

---
+++ code/preprocess.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pickle
+
+pre = '../data/cifar-10-batches-py/'
+
+
+def load(file):
+    with open(file, 'rb') as fo:
+        d = pickle.load(fo, encoding='bytes')
+    return d
+
+
+def main():
+    path = pre + 'data_batch_'
+
+    for i in range(1, 6):
+        whole = path + str(i)
+        d = load(whole)
+        print(len(d))
+        for ele in d:
+            print(ele)
+        print(d[ele][:10])
+        print(len(d[ele]))
+        print(type(d[ele]))
+        # exit()
+
+
+def build_training_set():
+    path = pre + 'data_batch_'
+    dataset, label = [], []
+    for i in range(1, 6):
+        whole = path + str(i)
+        dict = load(whole)
+        label.extend(list(dict[b'labels']))
+        dataset.extend(list(dict[b'data']))
+    dataset = np.array(dataset)
+    label = np.array(label)
+    print(dataset.shape, label.shape)
+
+    return dataset, label
+
+if __name__ == '__main__':
+    build_training_set()
